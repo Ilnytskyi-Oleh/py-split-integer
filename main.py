88cba772e6c98d3ad4eca17ff7a6b94257bb93f8
Oleh Ilnytskyi <ilnytskyy67922@gmail.com>
2025-11-03 18:45:10 +0000

    sol_v1

--- main.py
+++
@@ -1,8 +0,0 @@
-def split_integer(value: int, number_of_parts: int) -> list[int]:
-    base: int = value // number_of_parts
-    remainder: int = value % number_of_parts
-    parts: list[int] = [base] * number_of_parts
-    for i in range(remainder):
-        parts[number_of_parts - 1 - i] += 1
-    return sorted(parts)
-
